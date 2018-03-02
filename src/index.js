39b9935a319a8575b06ab87d90ce31ea985e566d
Alan Pieczonka <alanpieczonka@gmail.com>
2018-03-02 09:23:30 +0100

    Install google-images

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import express from 'express';
 import path from 'path';
-
+import GoogleImages from 'google-images';
+import dotenv from 'dotenv';
 const app = express();
 
-app.get('/*', (req,res) => {
+dotenv.config();
+
+const client = new GoogleImages(process.env.ENGINE_ID, process.env.API_KEY);
+
+
+app.get('/', (req,res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/images', (req,res) => {
+  client.search('Gojira').then((images) => res.json(images));
 });
 
 app.listen(8080, () => console.log('Running on port 8080'));
