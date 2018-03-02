133bb3b8834e4f44276c7b4efa9c4e33be068fe9
Alan Pieczonka <alanpieczonka@gmail.com>
2018-03-02 18:05:31 +0100

    Clean up index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+import indexRoute from './routes/index';
 import searchRoute from './routes/search';
-import path from 'path';
+import latestRoute from './routes/latest';
+
+dotenv.config();
+mongoose.connect(process.env.MONGODB_URL);
+
 const app = express();
-
-app.get('/', (req,res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
+app.use('/', indexRoute);
 app.use('/api/imagesearch', searchRoute);
+app.use('/api/latest', latestRoute)
 
 app.listen(8080, () => console.log('Running on port 8080'));
