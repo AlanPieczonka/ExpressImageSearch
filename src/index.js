a4bce5ec09c9960b0fd2ddad9a6ad1b519704f1b
Alan Pieczonka <alanpieczonka@gmail.com>
2018-03-02 11:20:28 +0100

    Add option to paginate results and do some refactoring

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import searchRoute from './routes/search';
-
+import path from 'path';
 const app = express();
 
 app.get('/', (req,res) => {
--- src/routes/search.js
+++ src/routes/search.js
@@ -7,9 +7,27 @@ const router = express.Router();
 dotenv.config();
 const client = new GoogleImages(process.env.ENGINE_ID, process.env.API_KEY);
 
-router.get('/:search', (req, res) => {
-  const search = req.params.search;
-  client.search(search).then((images) => res.json(images));
+router.get('/:search', async (req, res) => {
+  const { search } = req.params,
+        { offset } = req.query;
+
+  const images = await _getImages(search, offset);
+  res.json(images)
 });
+
+
+const _getImages = async(search, offset) => {
+  let result;
+  try {
+    result = await client.search(search)
+  } catch(err){
+    return result = { error: 'There has been an error!' };
+  }
+  return _paginate(result, offset);
+}
+
+const _paginate = (arr, offset) => {
+  return arr.slice(0, offset)
+}
 
 export default router;
