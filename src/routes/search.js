4239889fe668f5a41f0294bc534b60daae9e462f
Alan Pieczonka <alanpieczonka@gmail.com>
2018-03-02 18:06:03 +0100

    Refactor search route

--- src/routes/search.js
+++ src/routes/search.js
@@ -1,33 +1,37 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import GoogleImages from 'google-images';
+import mongoose from 'mongoose';
 
-const router = express.Router();
+import SearchTerm from '../models/SearchTerm';
+import  paginate  from '../helpers/paginate';
 
 dotenv.config();
 const client = new GoogleImages(process.env.ENGINE_ID, process.env.API_KEY);
 
+const router = express.Router();
 router.get('/:search', async (req, res) => {
   const { search } = req.params,
         { offset } = req.query;
+
+  const newSearchTerm = new SearchTerm({
+    searchTerm: search
+  });
+  newSearchTerm.save().then(response => console.log(response))
+                      .catch(error => console.log(error));
 
   const images = await _getImages(search, offset);
   res.json(images)
 });
 
-
-const _getImages = async(search, offset) => {
+const _getImages = async (search, offset) => {
   let result;
   try {
     result = await client.search(search)
   } catch(err){
     return result = { error: 'There has been an error!' };
   }
-  return _paginate(result, offset);
-}
-
-const _paginate = (arr, offset) => {
-  return arr.slice(0, offset)
+  return paginate(result, offset);
 }
 
 export default router;
